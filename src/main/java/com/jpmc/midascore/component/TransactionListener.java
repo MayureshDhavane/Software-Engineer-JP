715f0304464dc357b12bbd9a7ca969ded4f64111
MayureshDhavane <mayureshdhavane@gmail.com>
2026-04-13 19:45:27 +0530

    Implement transaction processing logic and add TransactionRecord entity and repository

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -4,12 +4,51 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.entity.User;
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.repository.UserRepository;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
+
+import java.util.Optional;
 
 @Component
 public class TransactionListener {
 
+    private final UserRepository userRepository;
+    private final TransactionRecordRepository transactionRecordRepository;
+
+    public TransactionListener(UserRepository userRepository,
+                               TransactionRecordRepository transactionRecordRepository) {
+        this.userRepository = userRepository;
+        this.transactionRecordRepository = transactionRecordRepository;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        System.out.println(transaction.getAmount());
+
+        Optional<User> senderOpt = userRepository.findById(transaction.getSenderId());
+        Optional<User> recipientOpt = userRepository.findById(transaction.getRecipientId());
+
+        if(senderOpt.isPresent() && recipientOpt.isPresent()) {
+
+            User sender = senderOpt.get();
+            User recipient = recipientOpt.get();
+
+            if(sender.getBalance() >= transaction.getAmount()) {
+
+                sender.setBalance(sender.getBalance() - transaction.getAmount());
+                recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+                userRepository.save(sender);
+                userRepository.save(recipient);
+
+                TransactionRecord record = new TransactionRecord();
+                record.setAmount(transaction.getAmount());
+                record.setSender(sender);
+                record.setRecipient(recipient);
+
+                transactionRecordRepository.save(record);
+            }
+        }
     }
 }
---
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -0,0 +1,47 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private float amount;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User recipient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+}
